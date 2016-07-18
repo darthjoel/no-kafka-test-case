17a120f0e3dde43ee84a1382720d48ceeecc6f42
Joel Confino <joel.confino@moven.com>
2016-07-18 14:29:11 -0400

    updated with _mapTopics timing

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 ## Intro
 
 I am running a system that processes about 3M messages per day.  It uses Node 0.10, Kafka 0.9 and high level consumers
-using no-kafka 2.5.6.  When fetch sizes get large, the consumers start to rebalance infinitely.  The culprit appears
-to be the synchronous call to deserialize a fetch response from 
-[within client.js](https://github.com/oleksiyk/kafka/blob/v2.5.6/lib/client.js#L337).  That call is taking 250ms-1.3
-secs with the test fetch responses included in this project.  The node event loop shouldn't be blocked for >250ms.
+using no-kafka 2.5.6.  When fetch sizes get large, the consumers start to rebalance infinitely due to event loop lag 
+spiking to high levels (over 1 sec).  The culprit appears to be the synchronous call to deserialize a fetch response from 
+[within client.js](https://github.com/oleksiyk/kafka/blob/v2.5.6/lib/client.js#L337) as well as the synchronous call
+to [_mapTopics](https://github.com/oleksiyk/kafka/blob/master/lib/client.js#L57).  Combined these 2 calls are taking
+2.7-32secs with the test fetch responses included in this project.  The node event loop cannot be blocked for that long
+or the consumer heartbeat requests not be sent on time.
 
 ## Running this test
     node index.js
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var Protocol = require('no-kafka/lib/protocol');
+var _ = require('lodash');
 
 //
 // This is test measures the time it takes to read a
 // FetchResponse of size 1MB and 6MB
-// It is copied from: https://github.com/oleksiyk/kafka/blob/v2.5.6/lib/client.js#L337
+// It is testing the synchronous functions (e.g. slow part) in the
+// Client.fetchRequest function: https://github.com/oleksiyk/kafka/blob/v2.5.6/lib/client.js#L313
 //
 
 var myProtocol = new Protocol({
@@ -13,15 +15,43 @@ var myProtocol = new Protocol({
 
 fs.readFile('fetch_response_1mb', function (err, data) {
   if (err) throw err;
-  var start = new Date().getTime();
-  console.log(myProtocol.read(data).FetchResponse().result.topics);
-  console.log('reading 1mb buffer took ' + (new Date().getTime() - start) + 'ms');
-
+  var label = "1MB";
+  var topics = readTopicsFromBuffer(data, label);
+  _mapTopics(topics, label);
   fs.readFile('fetch_response_6mb', function (err, data) {
     if (err) throw err;
-    var start = new Date().getTime();
-    console.log(myProtocol.read(data).FetchResponse().result.topics);
-    console.log('reading 6mb buffer took ' + (new Date().getTime() - start) + 'ms');
+    var label = "6MB";
+    var topics = readTopicsFromBuffer(data, label);
+    _mapTopics(topics, label);
   });
-
 });
+
+function readTopicsFromBuffer(data, label) {
+  var start = new Date().getTime();
+  // line copied directly from client.js
+  // https://github.com/oleksiyk/kafka/blob/v2.5.6/lib/client.js#L337
+  var topics = myProtocol.read(data).FetchResponse().result.topics;
+  console.log('reading ' + label + ' buffer took ' + (new Date().getTime() - start) + 'ms');
+  return topics;
+}
+
+// function copied directly from client.js, just added label and timing
+// https://github.com/oleksiyk/kafka/blob/master/lib/client.js#L57
+function _mapTopics(topics, label) {
+  var start = new Date().getTime();
+  var result = _(topics).flatten().transform(function (a, tv) {
+    if (tv === null) { return; } // requiredAcks=0
+    _.each(tv.partitions, function (p) {
+      a.push(_.merge({
+            topic: tv.topicName,
+            partition: p.partition
+          },
+          _.omit(p, 'partition')
+          /*function (_a, b) {if (b instanceof Buffer) {return b;}}*/) // fix for lodash _merge in Node v4: https://github.com/lodash/lodash/issues/1453
+      );
+    });
+    return;
+  }, []).value();
+  console.log('_mapTopics ' + label + ' took ' + (new Date().getTime() - start) + 'ms');
+  return result;
+}
